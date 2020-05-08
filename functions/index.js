9d1a1da3293e49420e22bbc2934618ab3b3c5711
AkshatSharma98 <akshatsharma9830@gmail.com>
2020-05-08 10:22:46 +0530

    Fixed UserJoined Cloud Functions

--- functions/index.js
+++ functions/index.js
@@ -32,19 +32,14 @@ exports.projectCreated = functions.firestore
     return createNotification(notification);
   });
 
-exports.userJoined = functions.auth.user().onCreate((user) => {
-  return admin
-    .firestore()
-    .collection("users")
-    .doc(user.uid)
-    .get()
-    .then((doc) => {
-      const newUser = doc.data();
-      const notification = {
-        content: "Joined the party",
-        user: `${newUser.firstName} ${newUser.lastName}`,
-        time: admin.firestore.FieldValue.serverTimestamp(),
-      };
-      return createNotification(notification);
-    });
-});
+exports.userJoined = functions.firestore
+  .document("users/{uid}")
+  .onCreate((doc) => {
+    const newUser = doc.data();
+    const notification = {
+      content: "Joined the party",
+      user: `${newUser.firstName} ${newUser.lastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp(),
+    };
+    return createNotification(notification);
+  });
